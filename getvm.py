d4a397664cd7b382f69409144c06404397664473
Siddarth Senthilkumar <sidsenkumar11@gmail.com>
2019-03-15 17:24:34 -0700

    Fixed getvm script

--- getvm.py
+++ getvm.py
@@ -1,6 +1,7 @@
 from swpag_client import Team
 t = Team("http://api.ictf2019.net/", "lVTU84h3IsWsv5Qa48Wv")
 vm_info = t.get_vm()
-print(vm_info['ictf_key'])
+print(vm_info.keys())
+print(vm_info['ctf_key'])
 print(vm_info['ip'])
-
+print(vm_info['team_id'])
